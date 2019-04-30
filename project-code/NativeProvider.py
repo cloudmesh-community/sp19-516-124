c53e66df24185213828f59b51357d53fcbed887b
garbeandy <46429660+garbeandy@users.noreply.github.com>
2019-04-30 06:34:46 -0400

    Create NativeProvider.py
    Slug for new last minute scope-request 4/30/2019 - https://piazza.com/class/jql1e30dxswzu?cid=527

---
+++ project-code/NativeProvider.py
@@ -0,0 +1,20 @@
+# New last minute scope-request 4/30/2019 - https://piazza.com/class/jql1e30dxswzu?cid=527
+
+#e.g. the location for it should be
+ 
+# https://github.com/cloudmesh/cloudmesh-cloud/tree/master/cloudmesh/compute/azure
+
+#I propose to rename this provider to 
+
+NativeProvider.py
+
+# to distinguish it from
+
+ 
+# AzProvider.py .  using az
+
+# Provider.py .     using libcloud
+
+ 
+
+# Naturally all student woring on Azure need to be testing all providers not just their own. But that is suposedly simple as the test shoul just be able to be run and the nbenchmark should speeak for itsel.
